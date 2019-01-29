1278a37f38814f166442bb05223ccad8eee040bb
Manoj V Sarangmath <manoj.kumar.v.sarangmath@sap.com>
2019-01-29 20:13:34 +0530

    first commit to gitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  username ='';
+  showSecret=false;
+  log=[];
+
+  handleButtonClick():void {
+    this.showSecret=!this.showSecret;
+    this.log.push(new Date());
+  }
+
 }
